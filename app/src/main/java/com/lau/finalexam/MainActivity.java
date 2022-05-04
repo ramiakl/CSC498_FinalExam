46a2c8f6eb250d37528f9d32554788f8dcaa5775
Rami <ramiakl33@gmail.com>
2022-05-04 12:31:07 +0300

    Creating a database and table

--- app/src/main/java/com/lau/finalexam/MainActivity.java
+++ app/src/main/java/com/lau/finalexam/MainActivity.java
@@ -2,6 +2,7 @@ package com.lau.finalexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try{
+
+            SQLiteDatabase sql = this.openOrCreateDatabase("laudb", MODE_PRIVATE, null);
+            sql.execSQL("CREATE Table IF NOT EXISTS courses (course_name VARCHAR, link VARCHAR)");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
