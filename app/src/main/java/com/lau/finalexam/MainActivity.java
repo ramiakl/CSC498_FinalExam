274cb648283f928630bb5bf0998fc29900912576
Rami <ramiakl33@gmail.com>
2022-05-04 12:36:36 +0300

    Adding all the courses to the database

--- app/src/main/java/com/lau/finalexam/MainActivity.java
+++ app/src/main/java/com/lau/finalexam/MainActivity.java
@@ -18,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase sql = this.openOrCreateDatabase("laudb", MODE_PRIVATE, null);
             sql.execSQL("CREATE Table IF NOT EXISTS courses (course_name VARCHAR, link VARCHAR)");
 
+            sql.execSQL("INSERT INTO courses(course_name, link) VALUES ('Finite Element Method', 'https://www.comsol.com/multiphysics/finite-element-method')");
+            sql.execSQL("INSERT INTO students(course_name, link) VALUES ('Engineering Entrepreneurship', 'https://byjus.com/commerce/what-is-entrepreneurship/#:~:text=Entrepreneurship%20is%20the%20ability%20and,the%20starting%20of%20new%20businesses.')");
+            sql.execSQL("INSERT INTO students(course_name, link) VALUES ('Europe and the middle east', 'https://www.britannica.com/')");
+            sql.execSQL("INSERT INTO students(course_name, link) VALUES ('Mobile Computing', 'https://ionicframework.com/')");
+
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
